81f48fb2f6b546c4d31fa3ad214e30b54296d1e0
Oscar <oscarbuzzsmith@gmail.com>
2020-10-09 13:40:40 +1100

    OS PROTOTYPE playermechanics 'implementing player lane change mechanics'

---
+++ project/Assets/SandBox/Oscar/prototypes/LanePrototype/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public float time = 0f;
+    public float dTime = 0.2f;
+    private int intLane = 0;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 targetPos;
+
+    public Transform leftLane;
+    public Transform middleLane;
+    public Transform rightLane;
+
+    public List<Transform> lanes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lanes.Add(leftLane);
+        lanes.Add(middleLane);
+        lanes.Add(rightLane);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        // moveable == 0 means the player may move at any point 
+
+        // on "A" or "LeftArrow" press
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (intLane != -1)
+            {
+                StopCoroutine(MoveDirection());
+                targetPos = new Vector3(pos.x, pos.y, lanes[intLane].position.z);
+                intLane--;
+                time += dTime;
+                StartCoroutine(MoveDirection());
+            }
+            else
+            {
+                Debug.Log("Illegal Move!");
+            }
+        }
+        // on "D" or "RightArrow" press
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (intLane != 1)
+            {
+                StopCoroutine(MoveDirection());
+                targetPos = new Vector3(pos.x, pos.y, lanes[intLane + 2].position.z);
+                intLane++;
+                time += dTime;
+                StartCoroutine(MoveDirection());
+            }
+            else
+            {
+                Debug.Log("Illegal Move!");
+            }
+        }
+    }
+
+    // move script
+    IEnumerator MoveDirection()
+    {
+        while (Vector3.Distance(transform.position, targetPos) > 0.05f)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, time);
+
+            
+
+            yield return null;
+        }
+        time = 0;
+        Debug.Log("Target Reached");
+    }
+
+}
