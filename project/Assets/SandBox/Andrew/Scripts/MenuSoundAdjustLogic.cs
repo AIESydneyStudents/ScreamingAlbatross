651b8ad8b0bb5f5567103865f1d69c49e601a7c3
suqnier <sl00ka@hotmail.com>
2020-10-22 10:21:40 +1100

    AS PROTOTYPE menu 'added sound options with variable transfer object'

---
+++ project/Assets/SandBox/Andrew/Scripts/MenuSoundAdjustLogic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class MenuSoundAdjustLogic : MonoBehaviour
+{
+    [SerializeField] ScriptableVolumeObject m_volumeObject;
+    [SerializeField] Slider m_silder;
+    [SerializeField] MenuButtons m_soundType;
+
+    public void UpdateVolume()
+    {
+        switch(m_soundType)
+        {
+            case MenuButtons.MasterVolume:
+                m_volumeObject.m_Master = m_silder.value;
+                break;
+            case MenuButtons.MusicVolume:
+                m_volumeObject.m_Music = m_silder.value;
+                break;
+            case MenuButtons.SFXVolume:
+                m_volumeObject.m_SFX = m_silder.value;
+                break;
+            case MenuButtons.None:
+                break;
+        }
+    }
+}
