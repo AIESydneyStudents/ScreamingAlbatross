b570df3586857e15f92c0d3e3587433f1e1abe0e
Suqnier <s200482@students.aie.edu.au>
2020-10-21 11:45:33 +1100

    AS PROTOTYPE unity 'menu screen first pass beginnings'

---
+++ project/Assets/SandBox/Andrew/Scripts/ButtonLogic.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonLogic : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] Text m_text;
+    [SerializeField] Color m_highlightColor;
+    Color m_defaultColour;
+    private void OnEnable()
+    {
+        m_defaultColour = m_text.color;
+    }
+    private void OnDisable()
+    {
+        m_defaultColour = Color.white;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        m_text.color = m_highlightColor;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        m_text.color = m_defaultColour;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+    }
+}
