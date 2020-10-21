d790e21791c92977b6e24b53f1c178a31d93c11e
Thomas Lamb <s200498@students.aie.edu.au>
2020-10-21 15:58:54 +1100

    TL Build 21/10/2020
    Basic Main menu, obstacle generation, importing of 3d models.

---
+++ project/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject left;
+    [SerializeField] GameObject middle;
+    [SerializeField] GameObject right;
+
+    [SerializeField] GameObject ObstaclePrefab;
+
+    //this is having a "limit" on the spawns, its max of 2 spawns per set
+    //interval so the obstacles at most spawn twice at a certain point so 
+    //the game isnt impossible at times.
+    [SerializeField] float spawnChargeTimer;
+    //when the spawnTimer hits the spawnchargetimer amount the spawncount will -1
+    //so another object can be spawned
+    [SerializeField] float spawnDelay;
+    float spawnTimer;
+    private int spawnCount = 3;
+
+    int laneSelect;
+
+    int leftRepeats = 0;
+    int rightRepeats = 0;
+    int middleRepeats = 0;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer > spawnChargeTimer)
+        {
+            spawnCount--;
+            spawnTimer = 0;
+        }
+
+        if (spawnCount <= 2)
+        {
+            spawnCount++;
+            laneSelect = Random.Range(0, 150);
+            if (laneSelect < 50 && leftRepeats < 2)
+            {
+                Instantiate(ObstaclePrefab, left.transform);
+                leftRepeats++;
+                rightRepeats = 0;
+                middleRepeats = 0;
+            }
+            if (laneSelect >= 50 && laneSelect < 100 && middleRepeats < 2)
+            {
+                Instantiate(ObstaclePrefab, middle.transform);
+                middleRepeats++;
+                leftRepeats = 0;
+                rightRepeats = 0;
+            }
+            if (laneSelect >= 100 && rightRepeats < 2)
+            {
+                Instantiate(ObstaclePrefab, right.transform);
+                rightRepeats++;
+                leftRepeats = 0;
+                middleRepeats = 0;
+            }
+        }
+    }
+}
