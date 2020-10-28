9ea7f3534742baa7bfa695dfc79253f2642e48e2
suqnier <sl00ka@hotmail.com>
2020-10-29 10:42:01 +1100

    AS PROTOTYPE play scene 'first pass on in game GUI'

---
+++ project/Assets/SandBox/Andrew/Scripts/PlayScene/PlaySceneScoreLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlaySceneScoreLogic : MonoBehaviour
+{
+    [SerializeField] Text m_text;
+    [SerializeField] ScriptableFloat m_scoreVariable;
+
+    private void OnEnable()
+    {
+        UpdateScore();
+    }
+
+    public void UpdateScore()
+    {
+        m_text.text = "Score: " + m_scoreVariable.m_Value.ToString();
+    }
+}
---
+++ project/Assets/SandBox/Andrew/Scripts/PlayScene/PlayTimeLogic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayTimeLogic : MonoBehaviour
+{
+    [SerializeField] Text m_text;
+    private float m_elapsedTime;
+    private void OnEnable()
+    {
+        m_elapsedTime = 0f;
+        UpdateTime();
+    }
+    private void FixedUpdate()
+    {
+        m_elapsedTime += Time.fixedDeltaTime;
+        UpdateTime();
+    }
+
+    private void UpdateTime()
+    {
+        m_text.text = (/*mins*/ ((int)(m_elapsedTime / 60f)).ToString("D2") + ":" /*secs*/ + ((int)m_elapsedTime).ToString("D2"));
+    }
+}
