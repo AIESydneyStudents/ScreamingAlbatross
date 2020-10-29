609328fd90fce8048365f70febc6e56324421696
suqnier <sl00ka@hotmail.com>
2020-10-29 15:12:05 +1100

    AS MAIN main 'merged various assets into main scenes'

--- project/Assets/SandBox/Andrew/Scripts/PlayScene/PlayUIController.cs
+++ project/Assets/SandBox/Andrew/Scripts/PlayScene/PlayUIController.cs
@@ -1,16 +1,30 @@
 ﻿using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayUIController : MonoBehaviour
 {
     [SerializeField] GameObject m_pauseContainer;
     [SerializeField] GameObject m_gameOverContainer;
 
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
     public void PauseGame()
     {
         //pause game
-
+        ResetTimeScale(true);
         //Display menu
         m_pauseContainer.SetActive(true);
+    }
+
+    public void UnpauseGame()
+    {
+        ResetTimeScale(false);
+        m_pauseContainer.SetActive(false);
     }
 
     public void GameOver()
@@ -19,5 +33,39 @@ public class PlayUIController : MonoBehaviour
 
         //display game over
         m_gameOverContainer.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        ResetTimeScale(false);
+        SceneManager.LoadScene(1);
+    }
+
+    public void PlayAgain()
+    {
+        ResetTimeScale(false);
+        SceneManager.LoadScene(2);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+
+    }
+
+    private void ResetTimeScale(bool pause)
+    {
+        if (pause)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
     }
 }
