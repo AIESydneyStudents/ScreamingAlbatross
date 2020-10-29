cce9241fbda462846278abae84a09a1181534693
Thomas Lamb <s200498@students.aie.edu.au>
2020-10-29 13:52:40 +1100

    Added road asset, generation of houses
    Still needs work do not run test.scene as build

---
+++ project/Assets/Scripts/EnvironmentSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject leftSpawner;
+    [SerializeField] GameObject rightSpawner;
+
+    [SerializeField] GameObject[] houses;
+
+    [SerializeField] float spawnRate = 2;
+
+    private float timerRight;
+    private float timerLeft;
+
+    
+
+    void Update()
+    {
+        timerRight += Time.deltaTime;
+        timerLeft += Time.deltaTime;
+
+        Debug.Log(timerRight);
+        Debug.Log(timerLeft);
+
+
+
+        if (timerRight > spawnRate)
+        {
+            Instantiate(houses[Random.Range(0, houses.Length)], rightSpawner.transform);
+            timerRight = 0;
+        }
+        if (timerLeft > spawnRate)
+        {
+            Instantiate(houses[Random.Range(0, houses.Length)], leftSpawner.transform.position,);
+            timerLeft = 0;
+        }
+        
+    }
+}
---
+++ project/Assets/Scripts/ThomasScript/RoadMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadMovement : MonoBehaviour
+{
+    [SerializeField] GameObject spawner;
+    [SerializeField] float m_moveSpeed = 10;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(m_moveSpeed * Time.deltaTime, 0, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.position = spawner.transform.position;
+    }
+}
