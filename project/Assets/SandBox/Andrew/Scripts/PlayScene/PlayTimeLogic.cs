9df40d811b9872c31203bb6a2168d54f0b1ed9f0
suqnier <sl00ka@hotmail.com>
2020-10-30 08:49:36 +1100

    AS FIX play scene 'fixed timer bug'

--- project/Assets/SandBox/Andrew/Scripts/PlayScene/PlayTimeLogic.cs
+++ project/Assets/SandBox/Andrew/Scripts/PlayScene/PlayTimeLogic.cs
@@ -5,9 +5,11 @@ public class PlayTimeLogic : MonoBehaviour
 {
     [SerializeField] Text m_text;
     private float m_elapsedTime;
+    private int m_elapsedMins;
     private void OnEnable()
     {
         m_elapsedTime = 0f;
+        m_elapsedMins = 0;
         UpdateTime();
     }
     private void FixedUpdate()
@@ -18,6 +20,11 @@ public class PlayTimeLogic : MonoBehaviour
 
     private void UpdateTime()
     {
-        m_text.text = (/*mins*/ ((int)(m_elapsedTime / 60f)).ToString("D2") + ":" /*secs*/ + ((int)m_elapsedTime).ToString("D2"));
+        if(m_elapsedTime >= 60f)
+        {
+            m_elapsedTime -= 60f;
+            m_elapsedMins++;
+        }
+        m_text.text = (/*mins*/ m_elapsedMins.ToString("D2") + ":" /*secs*/ + ((int)m_elapsedTime).ToString("D2"));
     }
 }
