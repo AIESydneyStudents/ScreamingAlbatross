d1aa164c3e3cdab7ade047e5a8fcb7c421e4f0e6
suqnier <sl00ka@hotmail.com>
2020-10-29 13:48:44 +1100

    AS PROTOTYPE unity 'setup of main scenes, and implementation of play UI'

---
+++ project/Assets/SandBox/Andrew/Scripts/PlayScene/PlayUIController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayUIController : MonoBehaviour
+{
+    [SerializeField] GameObject m_pauseContainer;
+    [SerializeField] GameObject m_gameOverContainer;
+
+    public void PauseGame()
+    {
+        //pause game
+
+        //Display menu
+        m_pauseContainer.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        //game over logic
+
+        //display game over
+        m_gameOverContainer.SetActive(true);
+    }
+}
