f37127a88693c5113031fafe1ed7e82212eab26e
Oscar <oscarbuzzsmith@gmail.com>
2020-10-30 13:49:01 +1100

    OS FEATURE unity 'working on new infinite generation'

--- project/Assets/Scripts/Environment/Stencil/StencilGeneration.cs
+++ project/Assets/Scripts/Environment/Stencil/StencilGeneration.cs
@@ -6,6 +6,8 @@ public class StencilGeneration : MonoBehaviour
 {
     private int spawner = 0;
 
+    [SerializeField] GameObject DefaultSpawn;
+
     [SerializeField] GameObject Stencil1;
     [SerializeField] GameObject Stencil2;
 
@@ -14,23 +16,49 @@ public class StencilGeneration : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if spawner is empty and a new stencil needs to be spawned
         if (spawner <= 0)
         {
+            // if stencils is empty
             if (Stencils.Count > 0)
             {
+                Vector3 newPos = Vector3.zero;
+
+                foreach (Transform child in Stencils[StencilsLength - 1].transform)
+                {
+                    if (child.name == "BACK")
+                    {
+                        newPos = child.position;
+                        break;
+                    }
+                }
+                // error tracing
+                if (newPos == Vector3.zero)
+                { Debug.Log("No Existing Stencils!"); }
+
                 StencilsLength++;
-                GameObject newStencil = Instantiate(Stencils[StencilsLength]);
+
+                //initialise and add new object to stencils list to track existing objects
+                GameObject newStencil = Instantiate(Stencil1);
+                newStencil.transform.position = newPos;
+                Stencils.Add(newStencil);
             }
             else
             {
+                Vector3 newPos = DefaultSpawn.transform.position;
                 StencilsLength++;
+                GameObject newStencil = Instantiate(Stencil2);
+                newStencil.transform.position = newPos;
+                Stencils.Add(newStencil);
             }
         }
     }
+
+
 }
