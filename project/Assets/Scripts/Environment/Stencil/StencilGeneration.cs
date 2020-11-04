fa79cb2814ff9a4105db34d8c486e09012bb14e7
Oscar <oscarbuzzsmith@gmail.com>
2020-11-04 14:44:09 +1100

    OS  FEATURE mechanics 'stencils now spawn at start up'

--- project/Assets/Scripts/Environment/Stencil/StencilGeneration.cs
+++ project/Assets/Scripts/Environment/Stencil/StencilGeneration.cs
@@ -6,17 +6,60 @@ public class StencilGeneration : MonoBehaviour
 {
     private int spawner = 0;
 
+    [SerializeField] GameObject SpawnOnStartPos;
     [SerializeField] GameObject DefaultSpawn;
 
     [SerializeField] List<GameObject> Stencils = new List<GameObject>();
 
     private int StencilsLength = 0;
     private List<GameObject> GameObjectStencils;
+
+
     // Start is called before the first frame update
     void Start()
     {
         GameObjectStencils = new List<GameObject>();
+
+
+        #region SPAWNING FILL STENCILS ON STARTUP
+
+        // spawns one stencil then spawn multiple until the playing feild is full, then proceeds to fill any empty spots as the game runs
+
+        Vector3 newPos = SpawnOnStartPos.transform.position;
+        StencilsLength++;
+        GameObject firstStencil = Instantiate(Stencils[Random.Range(0, Stencils.Count)]);
+        firstStencil.transform.position = newPos;
+        GameObjectStencils.Add(firstStencil);
+
+        StartCoroutine("OnStart");
     }
+
+    private IEnumerator OnStart()
+    {
+        while (spawner <= 0)
+        {
+            Vector3 newPos = Vector3.zero;
+
+            foreach (Transform child in GameObjectStencils[GameObjectStencils.Count - 1].transform)
+            {
+                if (child.name == "BACK")
+                {
+                    newPos = child.position;
+                    break;
+                }
+            }
+
+            //initialise and add new object to stencils list to track existing objects
+            StencilsLength++;
+            GameObject newStencil = Instantiate(Stencils[Random.Range(0, Stencils.Count)]);
+            newStencil.transform.position = newPos;
+            GameObjectStencils.Add(newStencil);
+
+            yield return null;
+        }
+    }
+
+    #endregion
 
     // Update is called once per frame
     void Update()
@@ -37,9 +80,9 @@ public class StencilGeneration : MonoBehaviour
                         break;
                     }
                 }
-                StencilsLength++;
 
                 //initialise and add new object to stencils list to track existing objects
+                StencilsLength++;
                 GameObject newStencil = Instantiate(Stencils[Random.Range(0, Stencils.Count)]);
                 newStencil.transform.position = newPos;
                 GameObjectStencils.Add(newStencil);
