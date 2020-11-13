d4bad866309db8547e9a83c194d62d06b8bdabdf
Oscar <oscarbuzzsmith@gmail.com>
2020-11-13 12:26:17 +1100

    OS FIX unity 'push before lunch, obstacles unfinished'

--- project/Assets/Scripts/Environment/ObstacleSpawner.cs
+++ project/Assets/Scripts/Environment/ObstacleSpawner.cs
@@ -11,16 +11,13 @@ public class ObstacleSpawner : MonoBehaviour
 
     [SerializeField] GameObject[] Obstacles;
     [SerializeField] int repeatAllowed;
-    
+
     [SerializeField] public ScriptableFloat spawnDelay;
     public float spawnTimer;
     public int spawnCount = 3;
+    private bool spawnnow = true;
 
     int laneSelect;
-
-    bool leftUsed = false;
-    bool rightUsed = false;
-    bool middleUsed = false;
 
     int leftRepeats = 0;
     int rightRepeats = 0;
@@ -28,33 +25,38 @@ public class ObstacleSpawner : MonoBehaviour
 
     void Update()
     {
-        spawnTimer += Time.deltaTime;
-
-        if (spawnTimer > (20f / spawnDelay.m_Value))
+        if (spawnCount <= 2)
         {
-            spawnCount--;
+            spawnTimer += Time.deltaTime;
+        }
+        else
+        {
             spawnTimer = 0;
         }
 
-        if (spawnCount <= 2)
+        if (spawnTimer >= (20f / spawnDelay.m_Value))
         {
-            spawnCount++;
-            laneSelect = Random.Range(0, 150);
-            if (laneSelect < 50 && leftRepeats < repeatAllowed)
+            spawnTimer = 0;
+            spawnnow = true;
+        }
+        else if (spawnnow)
+        {
+            laneSelect = Random.Range(1, 3);
+            if (laneSelect == 1 && leftRepeats < repeatAllowed)
             {
                 Instantiate(Obstacles[Random.Range(0, Obstacles.Length - 1)], left.transform);
                 leftRepeats++;
                 rightRepeats = 0;
                 middleRepeats = 0;
             }
-            if (laneSelect >= 50 && laneSelect < 100 && middleRepeats < repeatAllowed )
+            if (laneSelect == 2 && middleRepeats < repeatAllowed)
             {
                 Instantiate(Obstacles[Random.Range(0, Obstacles.Length - 1)], middle.transform);
                 middleRepeats++;
                 leftRepeats = 0;
                 rightRepeats = 0;
             }
-            if (laneSelect >= 100 && rightRepeats < repeatAllowed)
+            if (laneSelect == 3 && rightRepeats < repeatAllowed)
             {
                 Instantiate(Obstacles[Random.Range(0, Obstacles.Length - 1)], right.transform);
                 rightRepeats++;
@@ -64,6 +66,21 @@ public class ObstacleSpawner : MonoBehaviour
             }
         }
     }
-   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Obstacle")
+        {
+            spawnCount++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "Obstacle")
+        {
+            spawnCount--;
+        }
+    }
 
 }
