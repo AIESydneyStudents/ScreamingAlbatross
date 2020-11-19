f6ef013dfd471dcaf58f0ac4b157f830ab43e3a4
suqnier <sl00ka@hotmail.com>
2020-11-19 11:29:17 +1100

    AS FEATURE 'Powerup prefabs created'

---
+++ project/Assets/Scripts/PowerupScripts/WaveTranslation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class WaveTranslation : MonoBehaviour
+{
+    [SerializeField] Vector3 m_direction;
+    [SerializeField] float m_distance;
+
+    float f;
+
+    private void OnEnable()
+    {
+        f = 0f;
+    }
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + (m_direction.x * (float)(m_distance * Math.Sin(m_distance * f))), transform.position.y + (m_direction.y * (float)(m_distance * Math.Sin(m_distance * f))), transform.position.z + (m_direction.z * (float)(m_distance * Math.Sin(m_distance * f))));
+        f += .01f;
+    }
+}
