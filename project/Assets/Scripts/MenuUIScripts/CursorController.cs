eefa62e94ae9acbd04e2f22e794a675d2533e7d1
Suqnier <sl00ka@hotmail.com>
2020-11-24 08:04:13 +1100

    AS FEATURE 'Setup of cursor controller logic'

---
+++ project/Assets/Scripts/MenuUIScripts/CursorController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    [SerializeField] Texture2D m_idle, m_click;
+    private void OnEnable()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        Cursor.visible = false;
+        SetIdle();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            SetClick();
+        }
+    }
+
+    private void SetIdle()
+    {
+
+    }
+
+    private void SetClick()
+    {
+
+    }
+}
