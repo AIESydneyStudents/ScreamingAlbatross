a4e2752b0df1efaf599925b8ee4d1e1221f0a62e
Oscar <oscarbuzzsmith@gmail.com>
2020-11-26 14:49:20 +1100

    OS FEATURE mechanic 'capped obj speed'

--- project/Assets/Scripts/Player/DifficultySpeed.cs
+++ project/Assets/Scripts/Player/DifficultySpeed.cs
@@ -8,25 +8,36 @@ public class DifficultySpeed : MonoBehaviour
     private float speedTimer = 0.2f;
     private float timer;
     // speed increase per allocated tick
-    private float speedIncrease = 0.1f;
+    private float speedIncrease = 0.07f;
     // scriptable float linking to the global speed float
     public ScriptableFloat speed;
+
+    bool isRunning = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        isRunning = true;
         speed.m_Value = 20;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-
-        if (timer >= speedTimer)
+        if (isRunning)
         {
-            speed.m_Value += speedIncrease;
-            timer = 0;
+            timer += Time.deltaTime;
+
+            if (timer >= speedTimer)
+            {
+                speed.m_Value += speedIncrease;
+                timer = 0;
+            }
+            if (speed.m_Value > 80f)
+            {
+a                isRunning = false;
+            }
         }
+
     }
 }
