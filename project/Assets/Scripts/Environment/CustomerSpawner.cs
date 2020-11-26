985f8c888712113a1740f4d1de45eae4383ad26a
Thomas Lamb <s200498@students.aie.edu.au>
2020-11-26 11:42:52 +1100

    TL FEATURE Children prefabs working on both sides

--- project/Assets/Scripts/Environment/CustomerSpawner.cs
+++ project/Assets/Scripts/Environment/CustomerSpawner.cs
@@ -18,10 +18,21 @@ public class CustomerSpawner : MonoBehaviour
         spawnTimer += Time.deltaTime;
         if (spawnTimer >= spawnRate)
         {
+            
             if (Random.Range(0, 2) >= 1)
-                Instantiate(customerPrefab[Random.Range(0, customerPrefab.Length)], leftSidewalk.transform);
+            {
+                Instantiate(customerPrefab[Random.Range(0, customerPrefab.Length - 2)], leftSidewalk.transform);
+            }
             else
-                Instantiate(customerPrefab[Random.Range(0, customerPrefab.Length)], rightSidewalk.transform);
+            {
+                int customerToSpawn = Random.Range(0, customerPrefab.Length);
+                if (customerToSpawn == 4 || customerToSpawn == 5)
+                {
+                    Instantiate(customerPrefab[Random.Range(6, 9)], rightSidewalk.transform);
+                }
+                else
+                    Instantiate(customerPrefab[customerToSpawn], rightSidewalk.transform);
+            }
 
             spawnTimer = 0;
         }
